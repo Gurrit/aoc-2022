070987aa37b22aa83ff31ad16bff2241518a017d
GENGSMYR <gustav.engsmyre@volvocars.com>
2022-12-06 14:58:14 +0100

    i'm stupid (again)

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,6 +1,1 @@
-import java.io.File fun main(): Unit = println(File("input.txt").readLines().let { inp ->
-    if (System.getenv("part") != "part2")
-        (inp.takeWhile { !it.trim().startsWith("1") }.map { it.windowed(4,4, true) }.map { it.map { it.replace("[", "").replace("]", "").replace(" ", "") } }.toMutableList() to inp.takeLastWhile { it.isNotBlank() }.map { it.split(" ").filter { it.toIntOrNull() != null }.map { it.toInt() } })
-            .let { (i, ops) -> println(i); ops.fold(i) { acc, op -> println(acc); println(op); acc[op[2]-1] = acc[op[2]-1] + acc[op[1]-1].take(op[0]); acc[op[1]-1] = acc[op[1]-1].drop(op[0]); acc } }
-    else inp.map { 0 }
-} )
+import java.io.File fun main(): Unit = println(File("input.txt").readText().let { inp -> if (System.getenv("part") == "part2") inp.windowed(4, 1, false).asSequence().map { it.toList().distinct().size }.indexOfFirst { it == 4 } + 4 else inp.windowed(14, 1, false).asSequence().map { it.toList().distinct().size }.indexOfFirst { it == 14 } + 14 } )
